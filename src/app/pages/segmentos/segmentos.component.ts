0a59646626aa3930c3f34af7995f80b04f6e8986
vikler charles <charlesvikler@gmail.com>
2019-04-30 19:28:08 -0500

    mas ediciones

--- src/app/pages/segmentos/segmentos.component.ts
+++ src/app/pages/segmentos/segmentos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Segmento } from './models/segmento';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-segmentos',
@@ -6,8 +8,107 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./segmentos.component.css']
 })
 export class SegmentosComponent implements OnInit {
+  tipo: string;
+  segmentos: Segmento[] = [
+    {
+      nombre: 'reefer',
+      tipo: [
+        {
+          nombre: 'Reefer de 20 pies',
+          precio: '18.0000',
+          medidas: 'x medidas',
+          especificaciones: 'x especificaciones'
+        },
+        {
+          nombre: 'Reefer de 40 pies',
+          precio: '18.0000',
+          medidas: 'x medidas',
+          especificaciones: 'x expecificaciones'
+        }
+      ],
+    },
+    {
+      nombre: 'salas de ventas',
+      tipo: [
+        {
+          nombre: 'Salas de 20 pies',
+          precio: '18.0000',
+          medidas: 'x medidas',
+          especificaciones: 'x expecificaciones'
+        },
+        {
+          nombre: 'Salas de 40 pies',
+          precio: '18.0000',
+          medidas: 'x medidas',
+          especificaciones: 'x expecificaciones'
+        }
+      ],
+    },
+    {
+      nombre: 'bodegas',
+      tipo: [
+        {
+          nombre: 'Salas de 20 pies',
+          precio: '18.0000',
+          medidas: 'x medidas',
+          especificaciones: 'x expecificaciones'
+        },
+        {
+          nombre: 'Salas de 40 pies',
+          precio: '18.0000',
+          medidas: 'x medidas',
+          especificaciones: 'x expecificaciones'
+        }
+      ],
+    },
+    {
+      nombre: 'bodegas',
+      tipo: [
+        {
+          nombre: 'Salas de 20 pies',
+          precio: '18.0000',
+          medidas: 'x medidas',
+          especificaciones: 'x expecificaciones'
+        },
+        {
+          nombre: 'Salas de 40 pies',
+          precio: '18.0000',
+          medidas: 'x medidas',
+          especificaciones: 'x expecificaciones'
+        }
+      ],
+    },
+    {
+      nombre: 'proyectos arquitectonicos',
+      tipo: [
+        {
+          nombre: 'Salas de 20 pies',
+          precio: '18.0000',
+          medidas: 'x medidas',
+          especificaciones: 'x expecificaciones'
+        },
+        {
+          nombre: 'Salas de 40 pies',
+          precio: '18.0000',
+          medidas: 'x medidas',
+          especificaciones: 'x expecificaciones'
+        }
+      ],
+    }
+  ];
 
+  segmentoSeleccionado = new Segmento();
   constructor() { }
+
+  seleccionar(segmento: string) {
+    this.segmentoSeleccionado = this.segmentos.find((e) => e.nombre === segmento);
+    console.log(this.segmentoSeleccionado);
+  }
+
+  onChanges(e) {
+    console.log(e.target.value);
+    console.log(this.tipo);
+  }
 
   ngOnInit() {
   }
