f8375d11fb62dbe5d26c43131afd07484209491f
vikler charles <charlesvikler@gmail.com>
2019-05-02 10:00:58 -0500

    mas ediciones

--- src/app/pages/segmentos/segmentos.component.ts
+++ src/app/pages/segmentos/segmentos.component.ts
@@ -8,7 +8,14 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./segmentos.component.css']
 })
 export class SegmentosComponent implements OnInit {
-  tipoS: any;
+  tipoS = {
+    nombre: 'Nah',
+    precio: 'Nah',
+    medidas: 'Nah',
+    especificaciones: 'Nah'
+  };
+
+  segmento: any;
   segmentos: Segmento[] = [
     {
       nombre: 'reefer',
@@ -16,14 +23,14 @@ export class SegmentosComponent implements OnInit {
         {
           nombre: 'Reefer de 20 pies',
           precio: '18.0000',
-          medidas: 'x medidas',
-          especificaciones: 'x especificaciones'
+          medidas: 'x medidas de reefer 20 pies',
+          especificaciones: 'un contenedor mas pequeño'
         },
         {
           nombre: 'Reefer de 40 pies',
-          precio: '18.0000',
-          medidas: 'x medidas',
-          especificaciones: 'x expecificaciones'
+          precio: '20.0000',
+          medidas: 'x medidas reefer 40 pies',
+          especificaciones: 'un contenedor mas grande'
         }
       ],
     },
@@ -32,14 +39,14 @@ export class SegmentosComponent implements OnInit {
       tipo: [
         {
           nombre: 'Salas de 20 pies',
-          precio: '18.0000',
-          medidas: 'x medidas',
+          precio: '10.0000',
+          medidas: 'x medidas de salas de evntas de 20 pies',
           especificaciones: 'x expecificaciones'
         },
         {
           nombre: 'Salas de 40 pies',
-          precio: '18.0000',
-          medidas: 'x medidas',
+          precio: '19.0000',
+          medidas: 'x medidas de salas de evntas de 40 pies',
           especificaciones: 'x expecificaciones'
         }
       ],
@@ -48,31 +55,14 @@ export class SegmentosComponent implements OnInit {
       nombre: 'bodegas',
       tipo: [
         {
-          nombre: 'Salas de 20 pies',
-          precio: '18.0000',
+          nombre: 'Bodegas de 20 pies',
+          precio: '15.0000',
           medidas: 'x medidas',
           especificaciones: 'x expecificaciones'
         },
         {
-          nombre: 'Salas de 40 pies',
-          precio: '18.0000',
-          medidas: 'x medidas',
-          especificaciones: 'x expecificaciones'
-        }
-      ],
-    },
-    {
-      nombre: 'bodegas',
-      tipo: [
-        {
-          nombre: 'Salas de 20 pies',
-          precio: '18.0000',
-          medidas: 'x medidas',
-          especificaciones: 'x expecificaciones'
-        },
-        {
-          nombre: 'Salas de 40 pies',
-          precio: '18.0000',
+          nombre: 'Bodegas de 40 pies',
+          precio: '10.0000',
           medidas: 'x medidas',
           especificaciones: 'x expecificaciones'
         }
@@ -82,15 +72,15 @@ export class SegmentosComponent implements OnInit {
       nombre: 'proyectos arquitectonicos',
       tipo: [
         {
-          nombre: 'Salas de 20 pies',
-          precio: '18.0000',
+          nombre: 'Proyectos de 20 pies',
+          precio: '10.0000',
           medidas: 'x medidas',
           especificaciones: 'x expecificaciones'
         },
         {
-          nombre: 'Salas de 40 pies',
+          nombre: 'Proyectos de 40 pies',
           precio: '18.0000',
-          medidas: 'x medidas',
+          medidas: 'xx  medidas',
           especificaciones: 'x expecificaciones'
         }
       ],
@@ -101,13 +91,14 @@ export class SegmentosComponent implements OnInit {
   constructor() { }
 
   seleccionar(segmento: string) {
+    this.segmento = segmento;
     this.segmentoSeleccionado = this.segmentos.find((e) => e.nombre === segmento);
     console.log(this.segmentoSeleccionado);
   }
 
   onChanges(ev) {
     this.tipoS = this.segmentoSeleccionado.tipo.find((e) => e.nombre === ev.value);
-    console.log(this.tipoS);
+    console.log(this.tipoS) ;
   }
 
   ngOnInit() {
